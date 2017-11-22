a385d23396bebae60d7f4534386af457c6385818
Daniel Dolejška <dolejskad@gmail.com>
2017-11-22 11:05:58 +0100

    Změny v headeru pro generátor, nové funkce
    Do headeru generator.h byly přidány nové funkce
    Drobné úpravy dokumentací původních funkcí

--- src/generator/generator.h
+++ src/generator/generator.h
@@ -30,6 +30,10 @@
 //  DEKLARACE FUNKCÍ
 //======================================================================
 
+//-------------------------------------------------d-d-
+//  Instructions: Functions
+//-----------------------------------------------------
+
 /**
  * Zapíše instrukce značící začátek funkce. Po této funkci budou
  * následovat samotné instrukce dané funkce. Toto chování bude ukončeno
@@ -100,8 +104,28 @@ int Instruction_function_call(InstructionListPtr l, SymbolPtr symbol);
  */
 int Instruction_function_call_addParameter(InstructionListPtr l, SymbolPtr symbol);
 
+int Instruction_move(InstructionListPtr l, SymbolPtr variable, SymbolPtr symbol);
+
+int Instruction_createframe(InstructionListPtr l);
+
+int Instruction_pushframe(InstructionListPtr l);
+
+int Instruction_popframe(InstructionListPtr l);
+
+int Instruction_defvar(InstructionListPtr l, SymbolPtr symbol);
+
+int Instruction_call(InstructionListPtr l, SymbolPtr symbol);
+
+int Instruction_return(InstructionListPtr l);
+
+
+//-------------------------------------------------d-d-
+//  Instructions: Variables
+//-----------------------------------------------------
+
 /**
- * Zapíše instrukce pro deklaraci proměnné.
+ * Zapíše instrukce pro deklaraci proměnné. Nastaví její hodnotu na
+ * implicitní dle jejího typu.
  *
  * @param[in,out]   InstructionlistPtr  l       Ukazatel na existující seznam instrukcí
  * @param[in]       SymbolPtr           symbol  Symbol obsahující informace o proměnné
@@ -121,6 +145,22 @@ int Instruction_variable_declare(InstructionListPtr l, SymbolPtr symbol);
  */
 int Instruction_variable_assign(InstructionListPtr l, SymbolPtr variable, SymbolPtr symbol);
 
+
+//-------------------------------------------------d-d-
+//  Instructions: Stack
+//-----------------------------------------------------
+
+int Instruction_stack_push(InstructionListPtr l, SymbolPtr symbol);
+
+int Instruction_stack_pop(InstructionListPtr l, SymbolPtr symbol);
+
+int Instruction_stack_clear(InstructionListPtr l);
+
+
+//-------------------------------------------------d-d-
+//  Instructions: Math
+//-----------------------------------------------------
+
 /**
  * Zapíše instrukce pro součet proměnných/konstant.
  * V případě nutnosti provádí požadované konverze datových typů.
@@ -132,7 +172,7 @@ int Instruction_variable_assign(InstructionListPtr l, SymbolPtr variable, Symbol
  *
  * @retval  int Návratový kód popisující situaci (chyba, úspěch, ...)
  */
-int Instruction_math_add(InstructionListPtr l, SymbolPtr variable, SymbolPtr symbol1, SymbolPtr symbol2);
+int Instruction_add(InstructionListPtr l, SymbolPtr variable, SymbolPtr symbol1, SymbolPtr symbol2);
 
 /**
  * Zapíše instrukce pro rozdíl proměnných/konstant.
@@ -145,7 +185,7 @@ int Instruction_math_add(InstructionListPtr l, SymbolPtr variable, SymbolPtr sym
  *
  * @retval  int Návratový kód popisující situaci (chyba, úspěch, ...)
  */
-int Instruction_math_sub(InstructionListPtr l, SymbolPtr variable, SymbolPtr symbol1, SymbolPtr symbol2);
+int Instruction_sub(InstructionListPtr l, SymbolPtr variable, SymbolPtr symbol1, SymbolPtr symbol2);
 
 /**
  * Zapíše instrukce pro dělení proměnných/konstant.
@@ -158,7 +198,7 @@ int Instruction_math_sub(InstructionListPtr l, SymbolPtr variable, SymbolPtr sym
  *
  * @retval  int Návratový kód popisující situaci (chyba, úspěch, ...)
  */
-int Instruction_math_div(InstructionListPtr l, SymbolPtr variable, SymbolPtr symbol1, SymbolPtr symbol2);
+int Instruction_div(InstructionListPtr l, SymbolPtr variable, SymbolPtr symbol1, SymbolPtr symbol2);
 
 /**
  * Zapíše instrukce pro násobení proměnných/konstant.
@@ -171,7 +211,53 @@ int Instruction_math_div(InstructionListPtr l, SymbolPtr variable, SymbolPtr sym
  *
  * @retval  int Návratový kód popisující situaci (chyba, úspěch, ...)
  */
-int Instruction_math_mul(InstructionListPtr l, SymbolPtr variable, SymbolPtr symbol1, SymbolPtr symbol2);
+int Instruction_mul(InstructionListPtr l, SymbolPtr variable, SymbolPtr symbol1, SymbolPtr symbol2);
+
+
+//-------------------------------------------------d-d-
+//  Instructions: Math for stack
+//-----------------------------------------------------
+
+/**
+ * Zapíše instrukce pro součet proměnných/konstant na stacku.
+ *
+ * @param[in,out]   InstructionlistPtr  l   Ukazatel na existující seznam instrukcí
+ *
+ * @retval  int Návratový kód popisující situaci (chyba, úspěch, ...)
+ */
+int Instruction_add_stack(InstructionListPtr l);
+
+/**
+ * Zapíše instrukce pro rozdíl proměnných/konstant na stacku.
+ *
+ * @param[in,out]   InstructionlistPtr  l   Ukazatel na existující seznam instrukcí
+ *
+ * @retval  int Návratový kód popisující situaci (chyba, úspěch, ...)
+ */
+int Instruction_sub_stack(InstructionListPtr l);
+
+/**
+ * Zapíše instrukce pro dělení proměnných/konstant na stacku.
+ *
+ * @param[in,out]   InstructionlistPtr  l   Ukazatel na existující seznam instrukcí
+ *
+ * @retval  int Návratový kód popisující situaci (chyba, úspěch, ...)
+ */
+int Instruction_div_stack(InstructionListPtr l);
+
+/**
+ * Zapíše instrukce pro násobení proměnných/konstant na stacku.
+ *
+ * @param[in,out]   InstructionlistPtr  l   Ukazatel na existující seznam instrukcí
+ *
+ * @retval  int Návratový kód popisující situaci (chyba, úspěch, ...)
+ */
+int Instruction_mul_stack(InstructionListPtr l);
+
+
+//-------------------------------------------------d-d-
+//  Instructions: Logic
+//-----------------------------------------------------
 
 int Instruction_logic_eq(InstructionListPtr l, SymbolPtr variable, SymbolPtr symbol1, SymbolPtr symbol2);
 
@@ -185,36 +271,116 @@ int Instruction_logic_or(InstructionListPtr l, SymbolPtr variable, SymbolPtr sym
 
 int Instruction_logic_not(InstructionListPtr l, SymbolPtr variable, SymbolPtr symbol1, SymbolPtr symbol2);
 
-int Instruction_conversion_stri2int(InstructionListPtr l);
 
-int Instruction_conversion_int2float(InstructionListPtr l, SymbolPtr symbol);
+//-------------------------------------------------d-d-
+//  Instructions: Logic for stack
+//-----------------------------------------------------
 
-int Instruction_conversion_int2float(InstructionListPtr l, SymbolPtr symbol);
+int Instruction_logic_eq_stack(InstructionListPtr l);
 
-int Instruction_conversion_float2int(InstructionListPtr l, SymbolPtr symbol);
+int Instruction_logic_lt_stack(InstructionListPtr l);
 
-int Instruction_conversion_float2r2eint(InstructionListPtr l, SymbolPtr symbol);
+int Instruction_logic_gt_stack(InstructionListPtr l);
 
-int Instruction_conversion_float2r2oint(InstructionListPtr l, SymbolPtr symbol);
+int Instruction_logic_and_stack(InstructionListPtr l);
+
+int Instruction_logic_or_stack(InstructionListPtr l);
+
+int Instruction_logic_not_stack(InstructionListPtr l);
+
+
+//-------------------------------------------------d-d-
+//  Instructions: Conversions
+//-----------------------------------------------------
+
+int Instruction_stri2int(InstructionListPtr l);
+
+int Instruction_int2float(InstructionListPtr l, SymbolPtr symbol);
+
+int Instruction_int2float(InstructionListPtr l, SymbolPtr symbol);
+
+int Instruction_float2int(InstructionListPtr l, SymbolPtr symbol);
+
+int Instruction_float2r2eint(InstructionListPtr l, SymbolPtr symbol);
+
+int Instruction_float2r2oint(InstructionListPtr l, SymbolPtr symbol);
+
+int Instruction_stri2int_stack(InstructionListPtr l);
+
+int Instruction_int2float_stack(InstructionListPtr l);
+
+int Instruction_int2float_stack(InstructionListPtr l);
+
+int Instruction_float2int_stack(InstructionListPtr l);
+
+int Instruction_float2r2eint_stack(InstructionListPtr l);
+
+int Instruction_float2r2oint_stack(InstructionListPtr l);
+
+
+//-------------------------------------------------d-d-
+//  Instructions: IO
+//-----------------------------------------------------
 
 int Instruction_input(InstructionListPtr l, SymbolPtr symbol);
 
-int Instruction_print(InstructionListPtr l, SymbolPtr symbol);
+int Instruction_write(InstructionListPtr l, SymbolPtr symbol);
 
-int Instruction_print_addParameter(InstructionListPtr l);
 
-int Instruction_if_begin(InstructionListPtr l);
+//-------------------------------------------------d-d-
+//  Instructions: Strings
+//-----------------------------------------------------
 
-int Instruction_if_end(InstructionListPtr l);
+int Instruction_concat(InstructionListPtr l, SymbolPtr variable, SymbolPtr symbol1, SymbolPtr symbol2);
 
-int Instruction_if_else_begin(InstructionListPtr l);
+int Instruction_strlen(InstructionListPtr l, SymbolPtr variable, SymbolPtr symbol);
 
-int Instruction_if_else_end(InstructionListPtr l);
+int Instruction_getchar(InstructionListPtr l, SymbolPtr variable, SymbolPtr symbol1, SymbolPtr symbol2);
 
-int Instruction_loop_begin(InstructionListPtr l);
+int Instruction_setchar(InstructionListPtr l, SymbolPtr variable, SymbolPtr symbol1, SymbolPtr symbol2);
 
-int Instruction_loop_end(InstructionListPtr l);
 
+//-------------------------------------------------d-d-
+//  Instructions: Types
+//-----------------------------------------------------
+
+int Instruction_type(InstructionListPtr l, SymbolPtr variable, SymbolPtr symbol);
+
+
+//-------------------------------------------------d-d-
+//  Instructions: Jumps
+//-----------------------------------------------------
+
+int Instruction_label(InstructionListPtr l, char *label);
+
+int Instruction_jump(InstructionListPtr l, char *label);
+
+int Instruction_jumpifeq(InstructionListPtr l, char *label, SymbolPtr symbol1, SymbolPtr symbol2);
+
+int Instruction_jumpifneq(InstructionListPtr l, char *label, SymbolPtr symbol1, SymbolPtr symbol2);
+
+int Instruction_jumpifeq_stack(InstructionListPtr l, char *label);
+
+int Instruction_jumpifneq_stack(InstructionListPtr l, char *label);
+
+
+//-------------------------------------------------d-d-
+//  Instructions: Other
+//-----------------------------------------------------
+
+int Instruction_debug_break(InstructionListPtr l);
+
+int Instruction_debug_print(InstructionListPtr l, SymbolPtr symbol);
+
+/**
+ * Funkce pro zápis vlastní instrukce. Jendoduše uloží daný řetězec
+ * do seznamu instrukcí.
+ *
+ * @param[in,out]   InstructionlistPtr  l       Ukazatel na existující seznam instrukcí
+ * @param[in]       char                *data   Obsah instrukce
+ *
+ * @retval  int Návratový kód popisující situaci (chyba, úspěch, ...)
+ */
 int Instruction_custom(InstructionListPtr l, const char *data);
 
 /**
