9b84bbc58144ffc79726d68cd9e75aa40c0f905b
Daniel Dolejška <dolejskad@gmail.com>
2017-11-22 15:55:17 +0100

    Částečná implementace parseru a úpravy funkcí, které parser využívá

--- src/main/main.c
+++ src/main/main.c
@@ -1,10 +1,10 @@
 /**
- * Tento soubor obsahuje implementaci hlavní øídící funkce pøekladaèe.
+ * Tento soubor obsahuje implementaci hlavnÃ­ Å™Ã­dÃ­cÃ­ funkce pÅ™ekladaÄe.
  *
- * @author Daniel Dolejška (xdolej08)
+ * @author Daniel DolejÅ¡ka (xdolej08)
  * @date 21.11.2017
  * @project IFJcode17Parser
- * @subject Formální jazyky a pøekladaèe (IFJ) - FIT VUT v Brnì
+ * @subjec FormÃ¡lnÃ­ jazyky a pÅ™ekladaÄe (IFJ) - FIT VUT v BrnÄ›
  */
 
 #include <stdio.h>
@@ -12,6 +12,7 @@
 
 #ifdef DEBUG_INCLUDE
 #include "../generator/generator.h"
+#include "../generator/instruction_list.h"
 #include "../parser/parser.h"
 #include "../scanner/scanner.h"
 #include "../scanner/input.h"
@@ -19,11 +20,12 @@
 #include "../support/error_codes.h"
 #else
 #include "generator.h"
+#include "instruction_list.h"
 #include "parser.h"
-#include "parser_errors.h"
 #include "scanner.h"
 #include "input.h"
 #include "strings.h"
+#include "error_codes.h"
 #endif
 
 #ifdef DEBUG_PRINT_ENABLED
@@ -44,22 +46,26 @@
 #define DEBUG_ERR(...) do{ } while ( 0 )
 #endif
 
+char *error_description;
+
 //==================================================================d=d=
-//  DEKLARACE FUNKCÍ
+//  DEKLARACE FUNKCÃ
 //======================================================================
 
 /**
- * Hlavní funkce ovládající pøekladaè.
+ * HlavnÃ­ funkce ovlÃ¡dajÃ­cÃ­ pÅ™ekladaÄ.
  *
- * @param	int		argc	Poèet vstupních argumentù
- * @param	char    **argv	Pole vstupních argumentù
- * @param	char    **argv	Pole vstupních argumentù
+ * @param	int		argc	PoÄet vstupnÃ­ch argumentÅ¯
+ * @param	char    **argv	Pole vstupnÃ­ch argumentÅ¯
  */
-int main(int argc, char **argv)
+int main(/*int argc, char **argv*/)
 {
     DEBUG_LOG("main", "Starting program");
 
-    //  Inicializace potøebnıch promìnnıch
+
+    //-------------------------------------------------d-d-
+    //  Inicializace potÅ™ebnÃ½ch promÄ›nnÃ½ch
+    //-----------------------------------------------------
     DEBUG_LOG("main", "Initializing variables");
 
     int result = NO_ERROR;
@@ -68,28 +74,63 @@ int main(int argc, char **argv)
     if (input == NULL)
     {
         DEBUG_ERR("main", "failed to mallocate Input");
-        return INTERNAL_ERROR;
+        result = INTERNAL_ERROR;
+        goto program_exit;
     }
 
+    InstructionListPtr ilist = InstructionList_create();
+    if (ilist == NULL)
+    {
+        DEBUG_ERR("main", "failed to mallocate InstructionList");
+        result = INTERNAL_ERROR;
+        goto program_exit;
+    }
+
+    SymbolTablePtr symtable = SymbolTable_create();
+    if (symtable == NULL)
+    {
+        DEBUG_ERR("main", "failed to mallocate SymbolTable");
+        result = INTERNAL_ERROR;
+        goto program_exit;
+    }
+
+
+    //-------------------------------------------------d-d-
+    //  HlavnÃ­ logika programu
+    //-----------------------------------------------------
     DEBUG_LOG("main", "Starting parser");
-    int parser_result = Parser_ParseCode(input);
+    int parser_result = Parser_ParseInitial(input, ilist, symtable);
     if (parser_result != 0)
     {
-        DEBUG_PRINT("[%s] %3s Parser returner error code %i\n", "main", "ERR", parser_result);
+        DEBUG_PRINT("[%s] ERR Parser returned error code %i\n", "main", parser_result);
+        DEBUG_PRINT("[%s] ERR \tMessage: %s", "main", error_description);
+
+        String_destroy(&error_description);
         //  TODO: Output errors
         //  TODO: Cleanup & exit
     }
     else
     {
         DEBUG_LOG("main", "Outputting instructions");
-        //Instruction_outputAll();
+        //Instruction_outputAll(ilist);
     }
     result = parser_result;
 
 
-    //  Zrušení promìnnıch
+program_exit:
+    //-------------------------------------------------d-d-
+    //  ZruÅ¡enÃ­ promÄ›nnÃ½ch a ukonÄenÃ­ programu
+    //-----------------------------------------------------
     DEBUG_LOG("main", "Destroying allocated variables");
-    Input_destroy(&input);
+    if (input != NULL)
+        Input_destroy(&input);
+
+    if (ilist != NULL)
+        InstructionList_destroy(&ilist);
+
+    if (symtable != NULL)
+        SymbolTable_destroy(&symtable);
+
 
     DEBUG_LOG("main", "Exiting program");
     return result;
